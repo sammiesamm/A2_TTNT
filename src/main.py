f098ebdb844d8ce79f0d73b17d639d09301fa59e
OwO <thien.lee@hcmut.edu.vn>
2025-04-12 03:08:00 +0700

    update

--- src/main.py
+++ src/main.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-from board import Board
-from move_generator import generate_moves
-
-def main():
-    board = Board()
-    board.display()
-    moves = generate_moves(board)
-    print("Các nước đi hợp lệ từ vị trí hiện tại:", moves)
-
-if __name__ == '__main__':
-    main()
-=======
 from ui import UI
 if __name__=="__main__":
     UI().run()
->>>>>>> 2ff605b (update)
